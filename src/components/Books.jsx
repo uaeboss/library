5b75e0df90ecd24e8aa25f0e136b460524077d07
uaeboss <jeskemal@gmail.com>
2023-07-28 18:26:09 +0200

    added link to backend

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -1,27 +1,19 @@
 import { useState, useEffect } from "react";
-import { client } from "../client";
+// import { client } from "../client";
 import Popup from "reactjs-popup";
 
 function Books() {
-  const [books, setBooks] = useState([]);
+  const [books, setBooks] = useState(null);
   const [error, setError] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
 
-  async function getBookSlides() {
-    try {
-      const response = await client.getEntries({ content_type: "books" });
-      const books = response.items;
-      console.log(books);
-      setBooks(books);
-      setIsLoading(false);
-    } catch (error) {
-      console.log(error);
-      setIsLoading(false);
-    }
-  }
-
   useEffect(() => {
-    getBookSlides();
+    fetch("https://wbslibrarybackend.onrender.com/books")
+      .then((res) => res.json())
+      .then((allBooks) => {
+        setBooks(allBooks);
+        setIsLoading(false);
+      });
   }, []);
 
   if (isLoading) {
@@ -30,70 +22,71 @@ function Books() {
 
   return (
     <>
-      {books.map((entry) => {
-        const url = entry.fields.image.fields.file.url;
-        const key = entry.sys.id;
-        const title = entry.fields.title;
-        const description = entry.fields.description;
-        const subtitle = entry.fields.subtitle;
-        const author = entry.fields.author;
-        const genre = entry.fields.genre;
-        const isbn = entry.fields.isbn;
-        return (
-          <div key={key} className="card">
-            <div classname="cardtitle">
-              <div className="flip-box">
-                <div className="flip-box-inner">
-                  <div className="flip-box-front">
-                    <img src={url} alt={title} width="80%" />
-                  </div>
-                  <div className="flip-box-back">
-                    <div className="cardtitle">
-                      <h3 className="h3-bigger">{title}</h3>
-                      <p className="subtitle">{subtitle}</p>
+      {books &&
+        books.map((book) => {
+          return (
+            <div key={book.id} className="card">
+              <div classname="cardtitle">
+                <div className="flip-box">
+                  <div className="flip-box-inner">
+                    <div className="flip-box-front">
+                      <img src={book.image_url} alt={book.title} width="80%" />
                     </div>
-                    <div className="cardimage">
-                      <p className="p-overflow-hidden">{description}</p>
-                      <Popup
-                        trigger={<p className="openpopbut">mehr ...</p>}
-                        modal
-                        nested
-                      >
-                        {(close) => (
-                          <div className="modal">
-                            <div className="content">
-                              <div className="popupinfo">
-                              
-                              <h3 className="h3-bigger">{title}</h3>
-                              <p className="subtitle">{subtitle}</p>
-                              <h3>Autor/in: {author}</h3>
-                              <h3>Genre: {genre}</h3>
-                              <h3>ISBN: {isbn}</h3>
-                              </div>
-                              <div className="popupimg"><img src={url} alt={title} height="280px" /></div>
-                              <div className="popupdescrp"><p>{description}</p></div>
-                              
-                               
-                                
-                              <div className="popupbutton">
-                                <button className="popup-closeBtn" onClick={() => close()}>
-                                X
-                              </button></div>
-                              
-                            </div>
+                    <div className="flip-box-back">
+                      <div className="cardtitle">
+                        <h3 className="h3-bigger">{book.title}</h3>
+                        <p className="subtitle">{book.subtitle}</p>
+                      </div>
+                      <div className="cardimage">
+                        <p className="p-overflow-hidden">{book.description}</p>
+                        <Popup
+                          trigger={<p className="openpopbut">mehr ...</p>}
+                          modal
+                          nested
+                        >
+                          {(close) => (
+                            <div className="modal">
+                              <div className="content">
+                                <div className="popupinfo">
+                                  <h3 className="h3-bigger">{book.title}</h3>
+                                  <p className="subtitle">{book.subtitle}</p>
+                                  {/* <h3>Autor/in: {book.author}</h3> */}
+                                  <h3>Genre: {book.genre}</h3>
+                                  <h3>ISBN: {book.isbn}</h3>
+                                </div>
+                                <div className="popupimg">
+                                  <img
+                                    src={book.image_url}
+                                    alt={book.title}
+                                    height="280px"
+                                  />
+                                </div>
+                                <div className="popupdescrp">
+                                  <p>{book.description}</p>
+                                </div>
 
-                            <div></div>
-                          </div>
-                        )}
-                      </Popup>
+                                <div className="popupbutton">
+                                  <button
+                                    className="popup-closeBtn"
+                                    onClick={() => close()}
+                                  >
+                                    X
+                                  </button>
+                                </div>
+                              </div>
+
+                              <div></div>
+                            </div>
+                          )}
+                        </Popup>
+                      </div>
                     </div>
                   </div>
                 </div>
               </div>
             </div>
-          </div>
-        );
-      })}
+          );
+        })}
     </>
   );
 }
